7b8357f046554ad452b2e15af69a19a5097bc0db
mienord <mmnord@student.uia.no>
2024-09-18 13:09:57 +0200

    lagd css og knapper for index siden

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-<link rel="stylesheet" href="site/css/main.css?=v1.0">
-<div class="tabs">
+<link rel="stylesheet" href="site/css/main.css?=v3.0">
 
-  <input type="radio" name="tabs" id="tabtwo" checked="checked">
-  <label for="tabtwo">Rom Oversikt</label>
-  <div class="tab">
-    <h2>Tab Two Content</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
+<body>
 
-  <input type="radio" name="tabs" id="tabthree">
-  <label for="tabthree">Tab Three</label>
-  <div class="tab">
-    <h2>Tab Three Content</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  <div class="center-box">
+    <h1>Velkommen til Motel TM</h1>
+
+    <p>For å booke et rom hos oss er du nødt til å være logget inn.</p>
+    <a href="login.php">Logg inn</a>
+
+    <p>Har du ikke bruker hos oss? Registrer deg her!</p>
+    <a href="registrer.php">Registrer deg</a>
   </div>
-</div>
